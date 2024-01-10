335368a2ff114d5a284310d458557a7cd26f4c79
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2024-01-11 05:57:50 +0900

    Feat: 기본 폰트 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const MainPage = () => {
-  return <div className="text-faust">main page dfsdfasd</div>;
+  return (
+    <div className="text-faust">
+      <p className="font-title">타이틀</p>
+      <p className="font-gBold">볼드</p>
+      <p className="font-gLight">라이트</p>
+      <p className="font-number">121314135</p>
+      <p className="font-eng">english font</p>
+      <p className="font-content text-black">
+        본문용 글자입니다. 안녕하세요. 빵과 수프 크랲게
+      </p>
+    </div>
+  );
 };
 
 export default MainPage;
