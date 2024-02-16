3f7444c9f1ff6dab48f8541e9f2f8b327000e0ca
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2024-02-16 15:57:41 +0900

    Feat: 메인 페이지 기본 틀 생성
    - 유튜브
    - 바로가기 링크
    
    #11

---
+++ src/components/SiteLink.tsx
@@ -0,0 +1,52 @@
+import {
+  FaSquareArrowUpRight,
+  FaSquareXTwitter,
+  FaSquareYoutube,
+} from "react-icons/fa6";
+
+const SiteLink = () => {
+  return (
+    <ul>
+      {linkList.map((item) => (
+        <li className="py-1 hover:bg-primary-450">
+          <a href={item.link} className="flex gap-2 items-center">
+            {item.icon}
+            <span>{item.name}</span>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const linkList = [
+  {
+    name: "Project Moon 공식 트위터",
+    link: "https://twitter.com/ProjMoonStudio",
+    icon: <FaSquareXTwitter size={25} className="text-primary-500" />,
+  },
+  {
+    name: "Project Moon 공식 유튜브",
+    link: "https://www.youtube.com/channel/UCpqyr6h4RCXCEswHlkSjykA",
+    icon: <FaSquareYoutube size={25} className="text-primary-500" />,
+  },
+  {
+    name: "LCB 홈페이지",
+    link: "https://limbuscompany.kr",
+    icon: (
+      <img
+        src="https://encrypted-tbn0.gstatic.com/favicon-tbn?q=tbn:ANd9GcShROIHRbR4rWXlRoqw87jfeT3592yG1IN18E6titTVZNKsz9YNygkYhSM9e5Ts1xgDedkpjShwJIMaqDHd1mMn0GObkYg0hVPXAeMOndnEhZI1PpqaqKbEVQ"
+        alt="LCB favicon"
+        width={23}
+        height={23}
+      />
+    ),
+  },
+  {
+    name: "LCB 공식 트위터",
+    link: "https://twitter.com/LimbusCompany_B",
+    icon: <FaSquareXTwitter size={25} className="text-primary-500" />,
+  },
+];
+
+export default SiteLink;
