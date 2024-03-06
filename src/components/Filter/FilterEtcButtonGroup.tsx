03be08bc1aab52f470fb545a63dcffeb066bb8c0
Song Jihye <qwer15948@pusan.ac.kr>
2024-03-06 21:58:37 +0900

    Feat: 기타 키워드 버튼 선택 효과 추가
    #15

--- src/components/Filter/FilterEtcButtonGroup.tsx
+++ src/components/Filter/FilterEtcButtonGroup.tsx
@@ -1,3 +1,4 @@
+import useToggleButtons from "@hooks/useToggleButtons";
 import FilterEtcButton from "./FilterEtcButton";
 
 interface Content {
@@ -13,12 +14,21 @@ const FilterEtcButtonGroup = ({
   title,
   content,
 }: FilterEtcButtonGroupProps) => {
+  const [buttons, toggleButton] = useToggleButtons(
+    content.map((item) => item.name)
+  );
+
   return (
     <div>
       <span className="text-xs">{title}</span>
       <div className="grid grid-cols-3 gap-1.5">
-        {content.map((item) => (
-          <FilterEtcButton name={item.name} />
+        {content.map((item, index) => (
+          <FilterEtcButton
+            key={index}
+            name={item.name}
+            isSelected={buttons[index].isSelected}
+            onClick={() => toggleButton(item.name)}
+          />
         ))}
       </div>
     </div>
