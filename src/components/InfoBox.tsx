6821c686936f44894826e04f09ed2d7738b57d34
jihopark <jihodayo@gmail.com>
2024-03-08 00:16:25 +0900

    Feat: 인격 기본정보 반응형 추가 #17
    - 모바일 기준 collapse추가

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,5 +1,6 @@
-import { Typography } from "@material-tailwind/react";
-import React from "react";
+import { Button, Collapse, Typography } from "@material-tailwind/react";
+import React, { useState, useEffect } from "react";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
 interface InfoBoxProps {
   character: string;
@@ -18,6 +19,7 @@ interface InfoBoxProps {
 }
 
 const imagePaths = ["/assets/grade1.png", "/assets/grade2.png", "/assets/grade3.png"];
+const tempImg = "/assets/profile/donquixote.png";
 
 const InfoBox = ({
   character,
@@ -30,37 +32,87 @@ const InfoBox = ({
   resistance,
   status,
 }: InfoBoxProps) => {
+  const statusArray = [status.life, status.speed, status.defend];
+
+  const [open, setOpen] = useState(false);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 768);
+
+  const toggleOpen = () => setOpen((prevOpen) => !prevOpen);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const renderCollapseContent = () => {
+    return (
+      <div className="flex flex-col items-center" style={{ textAlign: "center" }}>
+        <Typography variant="small" className="p-1 md:text-xs flex" placeholder={status.life}>
+          {statusArray.map((item, index) => (
+            <span key={index} className="flex mr-2">
+              <img src={tempImg} alt="beforeImage" style={{ width: "14px", height: "14px" }} />
+              {item}
+            </span>
+          ))}
+        </Typography>
+
+        <Typography variant="small" className="p-1 md:text-xs flex" placeholder={resistance.join(", ")}>
+          {resistance.map((type, index) => (
+            <span
+              key={index}
+              className={`flex mr-2 ${type === "내성" ? "text-gray-400" : type === "취약" ? "text-red-500" : ""}`}
+            >
+              <img src={tempImg} alt="beforeImage" style={{ width: "14px", height: "14px" }} />
+              {type}{" "}
+            </span>
+          ))}
+        </Typography>
+
+        <Typography variant="small" className="p-1 md:text-xs" placeholder={affiliation}>
+          <span className="text-primary-100">소속</span> : {affiliation}
+        </Typography>
+
+        <Typography variant="small" className="p-1 md:text-xs" placeholder={releaseDate}>
+          <span className="text-primary-100">출시시기</span> : {releaseDate}
+        </Typography>
+
+        <Typography variant="small" className="p-1 md:text-xs" placeholder={obtainingMethod}>
+          {obtainingMethod}
+        </Typography>
+      </div>
+    );
+  };
+
   return (
-    <div className="bg-primary-500 text-white w-64 h-auto py-4 flex flex-col items-center gap-2">
-      <img src={afterProfileImage} alt="beforeImage" style={{ width: "40%", height: "auto" }} />
-      <img src={imagePaths[grade - 1]} alt="grade" style={{ width: "15%", height: "auto" }} />
+    <div className="bg-primary-500 text-white">
+      <div className="w-64 h-auto py-4 gap-2 flex flex-col items-center relative">
+        <img src={afterProfileImage} alt="beforeImage" style={{ width: "40%", height: "auto" }} />
+        <img src={imagePaths[grade - 1]} alt="grade" style={{ width: "15%", height: "auto" }} />
 
-      <p className="text-xl font-bold text-primary-100">{name}</p>
-      <p className="text-xl font-bold text-primary-100">{character}</p>
+        <p className="text-xl font-bold text-primary-100">{name}</p>
+        <p className="text-xl font-bold text-primary-100">{character}</p>
 
-      <Typography variant="small" className="p-1 md:text-xs" placeholder={affiliation}>
-        <span className="text-primary-100">소속</span> : {affiliation}
-      </Typography>
+        {/* Button to trigger collapse on smaller screens */}
+        <div className="md:hidden">
+          <Button
+            onClick={toggleOpen}
+            className="text-white flex items-center"
+            style={{ backgroundColor: "transparent", border: "none", boxShadow: "none", outline: "none" }}
+            placeholder={"button"}
+          >
+            상세정보
+            {open ? <FaArrowUp /> : <FaArrowDown />}
+          </Button>
+        </div>
+      </div>
 
-      <Typography variant="small" className="p-1 md:text-xs" placeholder={releaseDate}>
-        <span className="text-primary-100">출시시기</span> : {releaseDate}
-      </Typography>
-
-      <Typography variant="small" className="p-1 md:text-xs" placeholder={resistance.join(", ")}>
-        {resistance.map((type, index) => (
-          <span key={index} className={type === "내성" ? "text-gray-400" : type === "취약" ? "text-red-500" : ""}>
-            {type}{" "}
-          </span>
-        ))}
-      </Typography>
-
-      <Typography variant="small" className="p-1 md:text-xs" placeholder={status.life}>
-        {status.life} {status.speed} {status.defend}
-      </Typography>
-
-      <Typography variant="small" className="p-1 md:text-xs" placeholder={obtainingMethod}>
-        {obtainingMethod}
-      </Typography>
+      {/* Collapse content */}
+      {!isLargeScreen ? <Collapse open={open}>{renderCollapseContent()}</Collapse> : renderCollapseContent()}
     </div>
   );
 };
--- src/pages/IdentityDetailPage.tsx
+++ src/pages/IdentityDetailPage.tsx
@@ -5,18 +5,26 @@ import identity_data from "@constants/identity_detail.json";
 
 const IdentityDetailPage = () => {
   return (
-    <div>
-      <InfoBox
-        character={identity_data.character}
-        name={identity_data.name}
-        afterProfileImage={identity_data.afterProfileImage}
-        affiliation={identity_data.affiliation}
-        grade={identity_data.grade}
-        releaseDate={identity_data.releaseDate}
-        obtainingMethod={identity_data.obtainingMethod}
-        resistance={identity_data.resistance}
-        status={identity_data.status}
-      />
+    <div className="md:flex-row flex flex-col items-center">
+      <div>
+        <InfoBox
+          character={identity_data.character}
+          name={identity_data.name}
+          afterProfileImage={identity_data.afterProfileImage}
+          affiliation={identity_data.affiliation}
+          grade={identity_data.grade}
+          releaseDate={identity_data.releaseDate}
+          obtainingMethod={identity_data.obtainingMethod}
+          resistance={identity_data.resistance}
+          status={identity_data.status}
+        />
+        <div>
+          <h1 className="text-3xl font-bold text-primary-100">아이덴티티 상세 정보</h1>
+          <div>
+            <h2 className="text-2xl font-bold text-primary-100">아이덴티티 스킬</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
