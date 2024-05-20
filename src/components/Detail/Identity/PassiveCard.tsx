e19b427370d23101dfc14635ffdd8f87887977dd
Song Jihye <qwer15948@pusan.ac.kr>
2024-05-20 16:11:58 +0900

    Design: 패시브 발동 조건 간격 조정
    #32

--- src/components/Detail/Identity/PassiveCard.tsx
+++ src/components/Detail/Identity/PassiveCard.tsx
@@ -20,24 +20,24 @@ const PassiveCard = ({ type, synchronization, passive }: PassiveCardProps) => {
   return (
     <div className="p-3 bg-primary-500 mb-2">
       <div className="xl:flex xl:gap-3 items-center pb-6 ">
-        <div className={`text-xl font-bold pr-4 lg:p-0 pb-4 border-b-4 ${resourceColorMap[passive.resource]}`}>
+        <div
+          className={`text-xl font-bold pr-4 lg:pr-0 mb-2 border-b-4 ${
+            resourceColorMap[passive.resource]
+          }`}
+        >
           <span className="pr-4">{type}</span>
           <span>{passive.name}</span>
         </div>
-        <img
-          src={`/assets/resource/${passive.resource}.png`}
-          alt="resourceImg"
-          style={{
-            width: "auto",
-            height: "1.3em",
-            marginRight: "4px",
-            marginBottom: "2px",
-          }}
-          className="inline-block"
-        />
-        <span className="xl:pr-0 pr-2">X</span>
-        <span className="xl:pr-0 pr-2">{passive.resQuantity}</span>
-        <span className="">{passive.activeCond}</span>
+        <div className="flex gap-2">
+          <img
+            src={`/assets/resource/${passive.resource}.png`}
+            alt="resourceImg"
+            className="w-auto h-[1.3em] mb-0.5 inline-block"
+          />
+          <span>X</span>
+          <span>{passive.resQuantity}</span>
+          <span>{passive.activeCond}</span>
+        </div>
       </div>
       <KeywordHighlighted text={passive.effect} />
     </div>
