fe19f3a2170dee66bae6e32ca520b4384566ec27
Song Jihye <qwer15948@pusan.ac.kr>
2024-06-02 17:31:46 +0900

    Feat: 푸터 정보 수정
    - 이메일 입력
    - 유튜브 바로가기 버튼 삭제(추후 사용 가능성 있으므로 주석 처리함)
    - 깃헙 바로가기 링크 추가
    
    #38

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { IconButton, Typography } from "@material-tailwind/react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import nav from "@constants/nav.json";
 import { FaGithub, FaYoutube } from "react-icons/fa";
 
@@ -13,9 +13,10 @@ const Footer = () => {
         <div className="text-center md:text-left mt-2 text-primary-100">
           <p className="font-title text-md">단테의 빵과 수프</p>
           <p className="text-xs py-2 font-light">
-            팀 단테의 빵과 수프입니다.
+            팀 단테의 빵과 수프입니다. 문의사항이나 버그 제보는 아래로
+            연락해주세요.
             <br />
-            이메일@gmail.com
+            bas.limbus@gmail.com
           </p>
         </div>
         <div className="hidden md:flex mb-14">
@@ -42,11 +43,15 @@ const Footer = () => {
         />
         <div className="flex">
           <IconButton variant="text" placeholder={undefined}>
-            <FaGithub size={25} className="text-primary-200" />
+            <Link to="https://github.com/LimbusDeckMaker" target="_blank">
+              <FaGithub size={25} className="text-primary-200" />
+            </Link>
           </IconButton>
-          <IconButton variant="text" placeholder={undefined}>
+          {/* <IconButton variant="text" placeholder={undefined}>
+          <Link to="https://github.com/LimbusDeckMaker" target="_blank">
             <FaYoutube size={25} className="text-primary-200" />
-          </IconButton>
+          </Link>
+          </IconButton> */}
         </div>
       </div>
     </footer>
