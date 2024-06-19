b0e949dc5be902c036965c95b8f18846486868f2
jihopark <jihodayo@gmail.com>
2024-06-19 17:02:44 +0900

    Feat: nav바 로직 수정 #41
    페이지 이동시 nav바 닫는 로직 추가

--- src/components/layout/NavList.tsx
+++ src/components/layout/NavList.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-
+import React, { Dispatch, SetStateAction } from "react";
 import { Typography } from "@material-tailwind/react";
 import { NavLink } from "react-router-dom";
 import nav from "@constants/nav.json";
 
-const NavList = () => {
+interface NavListProps {
+  setIsNavOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const NavList: React.FC<NavListProps> = ({ setIsNavOpen }) => {
   return (
     <ul className="mt-2 mb-2 flex flex-col gap-2 md:mb-0 md:mt-0 md:flex-row md:items-center md:gap-6">
       {nav.map((item) => (
@@ -22,6 +25,7 @@ const NavList = () => {
                 ? "pb-1 border-b-2 border-b-primary-100 transition duration-500 ease-in-out"
                 : "bg-transparent border-b-2 border-transparent"
             }
+            onClick={() => setIsNavOpen(false)} // 페이지 이동 시 nav바를 닫음
           >
             {item.title}
           </NavLink>
