d1954d9af99783f5c8c9c95266e3548f24a5d0c0
jihopark <jihodayo@gmail.com>
2024-06-19 18:42:32 +0900

    Feat: nav 라인 클릭 시 페이지 이동 #41

--- src/components/layout/NavList.tsx
+++ src/components/layout/NavList.tsx
@@ -11,25 +11,28 @@ const NavList: React.FC<NavListProps> = ({ setIsNavOpen }) => {
   return (
     <ul className="mt-2 mb-2 flex flex-col gap-2 md:mb-0 md:mt-0 md:flex-row md:items-center md:gap-6">
       {nav.map((item) => (
-        <Typography
+        <NavLink
           key={`nav:${item.title}`}
-          as="li"
-          variant="small"
-          className="p-1 font-bold text-base md:text-sm text-primary-100"
-          placeholder={item.title}
-        >
-          <NavLink
-            to={item.link}
-            className={({ isActive }) =>
+          to={item.link}
+          className={({ isActive }) =>
+            `p-1 font-bold text-base md:text-sm text-primary-100 ${
               isActive
                 ? "pb-1 border-b-2 border-b-primary-100 transition duration-500 ease-in-out"
                 : "bg-transparent border-b-2 border-transparent"
-            }
-            onClick={() => setIsNavOpen(false)} // 페이지 이동 시 nav바를 닫음
+            }`
+          }
+          onClick={() => setIsNavOpen(false)} // 페이지 이동 시 nav바를 닫음
+        >
+          <Typography
+            key={`nav:${item.title}`}
+            as="li"
+            variant="small"
+            className="p-1 font-bold text-base md:text-sm text-primary-100"
+            placeholder={item.title}
           >
             {item.title}
-          </NavLink>
-        </Typography>
+          </Typography>
+        </NavLink>
       ))}
     </ul>
   );
