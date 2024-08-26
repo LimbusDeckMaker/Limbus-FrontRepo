b298e72c08ee06a30838012cbf1520c4631191ed
jihopark <jihodayo@gmail.com>
2024-08-26 17:26:06 +0900

    Feat: 마히스 3스 툴팁 제거

--- src/components/Detail/Identity/IdentitySkillCard.tsx
+++ src/components/Detail/Identity/IdentitySkillCard.tsx
@@ -113,64 +113,7 @@ const IdentitySkillCard = ({
           </div>
         </div>
       </div>
-      {currentSkill.name === "비탄하고, 애탄하며, 파탄하라" && (
-        <Tooltip
-          placement="right"
-          content={
-            <div>
-              <span className="text-xs">
-                데이터베이스 정해진 크기보다 와일드헌트 히스클리프의 '비탄하고,
-                애탄하며, 파탄하라' 스킬의 텍스트가 길어 표시되지 않은 부분이
-                있습니다.
-              </span>
-              <br />
-              <br />
-              <span className="text-xs"> 3동기화</span>
-              <br />
-              <span className="text-xs">
-                {" "}
-                [공격 종료시] 자신의 정신력이 0 미만이면, 정신력 10 회복.
-                정신력이 0보다 낮을수록 정신력 추가 회복 (차이 1당 1 회복, 최대
-                25)
-                <br />
-                [턴 종료시] 듀라한 제거
-              </span>
-              <br />
-              <br />
-              <span className="text-xs"> 4동기화</span>
-              <br />
-              <span className="text-xs">
-                {" "}
-                [공격 종료시] 자신의 정신력이 0 미만이면, 정신력 10 회복.
-                정신력이 0보다 낮을수록 정신력 추가 회복 (차이 1당 2 회복, 최대
-                50)
-                <br />
-                [턴 종료시] 듀라한 제거
-              </span>
-              <br />
-              <br />
-              <span className="text-xs">
-                빠르게 수정하도록 하겠습니다. 불편을 드려 죄송합니다.
-              </span>
-            </div>
-          }
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-            className="h-3.5 w-3.5 cursor-pointer text-primary-100 ml-0.5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-            />
-          </svg>
-        </Tooltip>
-      )}
+
       <div className="text-xs sm:text-sm">
         <KeywordHighlighted text={currentSkill.normalEffect} />
         <div>
