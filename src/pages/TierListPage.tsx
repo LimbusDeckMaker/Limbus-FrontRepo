d37f682eb09957e82f3b0474766098a1f86685fb
Song Jihye <qwer15948@pusan.ac.kr>
2024-08-27 11:31:52 +0900

    Docs: 티어표 설명 수정

--- src/pages/TierListPage.tsx
+++ src/pages/TierListPage.tsx
@@ -9,7 +9,8 @@ const TierListPage = () => {
           리세마라 티어표
         </h1>
         <p className="font-sansLight text-white text-xs md:text-sm">
-          - 본 티어표는 <span className="underline">새로 시작하는 관리자</span>
+          - 림버스 컴퍼니 최신 리세마라 티어표는{" "}
+          <span className="underline">새로 시작하는 관리자</span>
           에게 도움이 되도록 초반 컨텐츠에서의 활용도 및 인격 사용 난이도를
           기준으로 제작되었습니다.{" "}
           <span className="underline">전체 컨텐츠의 성능 기준이 아님</span>을
